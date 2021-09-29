e37e6db2c5241eb5b8792944d3465297dd9dd842
mcastegnaro <matheuscastegnaro@hotmail.com>
2021-09-29 08:57:49 -0300

    Corrigindo cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
@@ -5,17 +6,18 @@ import swaggerUi from "swagger-ui-express";
 import "./database";
 
 import "./shared/container";
+
 import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
 const app = express();
-const port = 3000;
 
+const port = 3333;
+
+app.use(cors());
 app.use(express.json());
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
 app.use(router);
 
 app.use(
